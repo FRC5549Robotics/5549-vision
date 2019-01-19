8c0a3b82a32b2ec7cb7a2242bc1ec220e3c7eca3
TyrannousHail03 <TyrannousHail03@users.noreply.github.com>
2019-01-18 21:17:14 -0500

    Fixed OpenCV Error and Added Program Timing
    Fixed OpenCV Error that would crash the program. Crash text looked like "OpenCV error : error: (-215) scn == 3 || scn == 4 in function cvtColor". Added in timing mechanisms for VideoWrite.py and VideoRead.py. In VideoWrite.py both Execution Time and Recording Time are printed. In VideoRead.py Playback Time is printed.

--- VideoRead.py
+++ VideoRead.py
@@ -1,18 +1,21 @@
 import numpy as np
 import cv2
+import timeit
 
 cap = cv2.VideoCapture("/home/nvidia/Documents/output.avi")
-
+start = timeit.default_timer()
 while cap.isOpened():
     ret, frame = cap.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame', gray)
-    cv2.waitKey(75)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    if ret is True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+        cv2.waitKey(116)
+    else:
+        break
 
+end = timeit.default_timer()
 cap.release()
 cv2.destroyAllWindows()
+print("Playback Time:" + str(end - start))
--- VideoWrite.py
+++ VideoWrite.py
@@ -1,11 +1,15 @@
 import cv2
+import timeit
+
+totaltime = timeit.default_timer()
 
 cap = cv2.VideoCapture("/dev/video1")
 fourcc = cv2.VideoWriter_fourcc(*'X264')
-out = cv2.VideoWriter('output.avi', fourcc, 60, (640, 480), True)
+out = cv2.VideoWriter('output.avi', fourcc, 30, (640, 480), True)
 
 framenum = 0
 
+start = timeit.default_timer()
 while True:
     ret, frame = cap.read()
     if ret == True:
@@ -22,6 +26,9 @@ while True:
     else:
         break
 
+end = timeit.default_timer()
+print("Total Execution Time: " + str(end-totaltime))
+print("Recording Time: " + str(end-start))
 cap.release()
 out.release()
 cv2.destroyAllWindows()
